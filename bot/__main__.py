cc8eb954987346fefadea26fd3581db08fa95349
gabrierr <82598985+gabrierr@users.noreply.github.com>
2021-10-22 01:01:53 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -5,6 +5,8 @@ from bot.helper.telegram_helper.message_utils import *
 from .helper.telegram_helper.filters import CustomFilters
 from .modules import authorize, list
 
+from telegram import InlineKeyboardMarkup
+from bot.helper.telegram_helper import button_builder
 
 def start(update, context):
     start_string = '\x1fPanggil aku Detective Minami, Tugasku adalah mencari File/Folder di Drive Pribadi\x1f'
@@ -14,6 +16,10 @@ def start(update, context):
 def log(update, context):
     sendLogFile(context.bot, update)
 
+def index(update, context):
+    sendMessage('Akses Gabrier Index Drive', context.bot, update)
+    buttons = button_builder.ButtonMaker()
+    buttons.buildbutton("Index Drive", f"https://view.gabriersite.workers.dev/0:/")
 
 botcmds = [(f'{BotCommands.ListCommand}','Mencari File di Drive')]
 
@@ -23,9 +29,11 @@ def main():
 
     start_handler = CommandHandler(BotCommands.StartCommand, start, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
     log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter, run_async=True)
+    list_handler = CommandHandler(BotCommands.IndexCommand, list_drive,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(log_handler)
+    dispatcher.add_handler(index_handler)
 
     updater.start_polling()
     LOGGER.info("Bot Berjalan!")
