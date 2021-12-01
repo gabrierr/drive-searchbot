5b3028c22fff8e8b3ad4b2023e1909bada366525
gabrierr <82598985+gabrierr@users.noreply.github.com>
2021-12-01 23:52:09 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -19,19 +19,11 @@ def index(update, context):
     buttons = button_builder.ButtonMaker()
     buttons.buildbutton("Index Frost", f"https://view.gabriersite.workers.dev/0:/")
     buttons.buildbutton("Index Zukky", f"https://media.gabriersite.workers.dev/0:/")
+    buttons.buildbutton("Odrive", f"https://www.odrive.com/s/30ff3fb5-45d4-4563-b48c-fc174f9324bf-61a7a7cb")
     buttons.buildbutton("Team Drive", f"https://groups.google.com/u/1/g/jiwa-laknat")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(3))
-    drive_string = f'''Akses ke Gabrier Index or join Team Drive ⇩'''
+    drive_string = f'''⇩ Akses ke Gabrier Index or join Team Drive ⇩'''
     sendMarkup(drive_string, context.bot, update, reply_markup)
-
-def odrive(update, context):
-    buttons = button_builder.ButtonMaker()
-    buttons.buildbutton("Drive Frost", f"https://www.odrive.com/s/7f86e862-11cf-4b83-9f2e-57c3cb89ef98-61a79a31")
-    buttons.buildbutton("Drive Zukky", f"https://www.odrive.com/s/2ba643fc-1e29-4382-9cd9-fa6cd6e8b49e-61a79a48")
-    buttons.buildbutton("Drive XDMedia (FLAC)", f"https://www.odrive.com/s/e1829671-3a48-4c93-9c18-b69d91940454-61a79a78")
-    reply_markup = InlineKeyboardMarkup(buttons.build_menu(3))
-    odrive_string = f'''Akses ke Odrive ⇩'''
-    sendMarkup(odrive_string, context.bot, update, reply_markup)
     
 botcmds = [(f'{BotCommands.ListCommand}','Mencari File di Drive')]
 
@@ -42,12 +34,10 @@ def main():
     start_handler = CommandHandler(BotCommands.StartCommand, start, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
     log_handler = CommandHandler(BotCommands.LogCommand, log, filters=CustomFilters.owner_filter, run_async=True)
     index_handler = CommandHandler(BotCommands.IndexCommand, index, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
-    odrive_handler = CommandHandler(BotCommands.OdriveCommand, odrive, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(log_handler)
     dispatcher.add_handler(index_handler)
-    dispatcher.add_handler(odrive_handler)
 
     updater.start_polling()
     LOGGER.info("Bot Berjalan!")
